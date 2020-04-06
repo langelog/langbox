31de46ee9e8a0efc018bb5f7626b90f67b6fa7a3
Angelo <langelog@me.com>
2020-04-06 10:41:37 -0400

    removed extras

--- extras/medium.bash
+++
@@ -1,7 +0,0 @@
-# Remember to first cd to your working directory
-# ...or change the $PWD to the directory that you want to use
-
-$ docker run --rm -v $PWD:/app -p 1314:1314 \
-  langelog/langbox:0.1.4_base \
-  root --notebook --ip 0.0.0.0 --port 1314 --allow-root
-  
